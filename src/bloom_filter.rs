2705482c8643380c949232e7581a974d4998aa6f
Lukas Valatka <lukas.valatka123@gmail.com>
2023-12-18 15:21:55 +0100

    add bitvec lib to scale filter size

--- src/bloom_filter.rs
+++ src/bloom_filter.rs
@@ -1,12 +1,15 @@
 use sha2::{Sha256, Digest};
 use shabal::{Shabal256, Shabal192};
+// use bit_set::BitSet;
+use bitvec::prelude::*;
 
 pub struct BloomFilter {
-    bitset: u8,
+    bitset: BitVec<u8, Msb0>,
+    size: usize
 }
 
 impl BloomFilter {
-    fn get_buckets(val: &str) -> Vec<u8> {
+    fn get_buckets(&self, val: &str) -> Vec<usize> {
         let mut hasher = Sha256::new();
         hasher.update(val);
         let hash = hasher.finalize();
@@ -29,40 +32,42 @@ impl BloomFilter {
             hash_int_2 = (hash_int_2 << 8) + hash2[i] as u128;
         }
 
-        let bucket_2 = hash_int_2 % 8;
+        let bucket_2 = hash_int_2 % self.size as u128;
 
         let mut buckets = Vec::new();
-        buckets.push(bucket as u8);
-        buckets.push(bucket_2 as u8);
+        buckets.push(bucket as usize);
+        buckets.push(bucket_2 as usize);
 
         return buckets;
 
     }
 
-    pub fn new() -> Self {
+    pub fn new(size: usize) -> Self {
         BloomFilter {
-            bitset: 0
+            bitset: bitvec![u8, Msb0; 0; size],
+            size: size
         }
     }
 
     pub fn set(&mut self, val: &str) {
-        let buckets = BloomFilter::get_buckets(val);
+        let buckets = self.get_buckets(val);
 
         for bucket in buckets {
             // getting the right bit
             // duplication
-            let mut mask: u8 = 1;
+            // let mut mask: u8 = 1;
 
-            for i in 0..bucket {
-                mask = mask << 1;
-            }
+            // for i in 0..bucket {
+            //     mask = mask << 1;
+            // }
 
-            self.bitset = self.bitset | mask;
+            // self.bitset = self.bitset | mask;
+            self.bitset.set(bucket, true)
         }
     }
 
     pub fn get(&self, val: &str) -> bool {
-        let buckets = BloomFilter::get_buckets(val);
+        let buckets = self.get_buckets(val);
 
         // println!("{:?}", buckets);
 
@@ -70,15 +75,17 @@ impl BloomFilter {
 
         for bucket in buckets {
             // getting the right bit
-            let mut mask: u8 = 1;
+            // let mut mask: u8 = 1;
 
-            for i in 0..bucket {
-                mask = mask << 1;
-            }
+            // for i in 0..bucket {
+            //     mask = mask << 1;
+            // }
 
             // if even a single bit is not set, it means for sure the value hasn't been added and in the
             // process hashed by bloom filter
-            exists &= ((mask & self.bitset) == mask)
+            // exists &= ((mask & self.bitset) == mask)
+
+            exists &= self.bitset.get(bucket).as_deref().unwrap();
         }
 
         return exists;
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::result;
 use rand::distributions::{Alphanumeric, DistString};
 use rand::{thread_rng, Rng};
 use std::mem;
+use bitvec::prelude::*;
 
 pub mod bloom_filter;
 
@@ -13,15 +14,30 @@ fn generate_random_string(length: usize) -> String {
 }
 
 fn main() {
+
+
+    // let b: BitArray = bitarr![2; 2];
+    // assert_eq!(b.len(), mem::size_of::<usize>() * 8);
+    // println!("{}", )
+
+    // let mut heap: BitVec<_, _> = BitVec::<Local, Word>::with_capacity(64);
+
+    // let mut s = BitSet::new();
+
+
+    // let mut bv = bitvec![u8, Msb0; 0; 5];
+    // bv.set(3, true);
+    // println!("{bv}");
+    
+    // println!("mem: {:?}", mem::size_of_val(bits));
+
     // Create a bitset. It's technically a byte array, but in which one operates with individuals bit individually
     // for now hardcoded to 8
-    let mut bloom_filter = bloom_filter::BloomFilter::new();
+    // println!("{}", bloom_filter.get("bla_bla"));
+    // // so if it says false, then it means 100% it's not in the set. true means it might be there, but not guaranteed.
+    // println!("{}", bloom_filter.get("bla_bla_6"));
 
-    println!("{}", bloom_filter.get("bla_bla"));
-    // so if it says false, then it means 100% it's not in the set. true means it might be there, but not guaranteed.
-    println!("{}", bloom_filter.get("bla_bla_6"));
-
-    println!("{}", generate_random_string(64));
+    // println!("{}", generate_random_string(64));
 
     // set candidates
     let mut candidates: Vec<String> = Vec::new();
@@ -29,25 +45,45 @@ fn main() {
     const CANDIDATES: usize = 50_000;
 
     for _ in 0..CANDIDATES {
-        candidates.push(generate_random_string(64));
+        candidates.push(generate_random_string(24));
     }
 
-    // println!("mem: {:?}", mem::size_of_val(&candidates));
+    // // println!("mem: {:?}", mem::size_of_val(&candidates));
+    // // goal - reach FP rate of 0.001, with 5k items
+    
+    // // half insert
+    // // for i in 0..CANDIDATES/2 {
+    // //     let mut false_positives = 0;
 
-    // half insert
+    // //     for i in CANDIDATES/2..CANDIDATES {
+    // //         let existance = bloom_filter.get(candidates.get(i).unwrap());
+
+    // //         if existance {
+    // //             false_positives += 1;
+    // //         }
+    // //     }
+
+    // //     let fp_rate = false_positives as f32 / ((CANDIDATES/2) as f32) * 100.0;
+    // //     println!("Size: {i}, FP rate: {fp_rate}%");
+    // //     bloom_filter.set(candidates.get(i).unwrap());
+    // // }
+
+    // // set half, test other half
+    let mut bloom_filter = bloom_filter::BloomFilter::new(50000);
+
     for i in 0..CANDIDATES/2 {
-        let mut false_positives = 0;
-
-        for i in CANDIDATES/2..CANDIDATES {
-            let existance = bloom_filter.get(candidates.get(i).unwrap());
-
-            if existance {
-                false_positives += 1;
-            }
-        }
-
-        let fp_rate = false_positives as f32 / ((CANDIDATES/2) as f32) * 100.0;
-        println!("Size: {i}, FP rate: {fp_rate}%");
         bloom_filter.set(candidates.get(i).unwrap());
     }
+
+    let mut false_positives = 0;
+    for i in CANDIDATES/2..CANDIDATES {
+        let existance = bloom_filter.get(candidates.get(i).unwrap());
+
+        if existance {
+            false_positives += 1;
+        }
+    }
+
+    let fp_rate = false_positives as f32 / ((CANDIDATES/2) as f32) * 100.0;
+    println!("Size: 5000, FP rate: {fp_rate}%");
 }

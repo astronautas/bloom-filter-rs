b8e0ce99a3783d3f6b9f67640a243a6c4c3fbdd8
Lukas Valatka <lukas.valatka123@gmail.com>
2023-12-29 21:21:55 +0200

    wip

--- src/bloom_filter.rs
+++ src/bloom_filter.rs
@@ -1,11 +1,11 @@
-use sha2::{Sha256, Digest};
-use shabal::{Shabal256, Shabal192};
+use sha2::{Digest, Sha256};
+use shabal::{Shabal192, Shabal256};
 // use bit_set::BitSet;
 use bitvec::prelude::*;
 
 pub struct BloomFilter {
     bitset: BitVec<u8, Msb0>,
-    size: usize
+    size: usize,
 }
 
 impl BloomFilter {
@@ -16,8 +16,8 @@ impl BloomFilter {
 
         let mut hash_int: u128 = 0;
 
-        // hashing lib returns array of bytes, convert into number
-        // slicing of first 128 bits is often more than enough to ensure uniformity
+        // hash is 32 bytes, so we need to convert it to a u128 - 16 bytes (128 bits)
+        // 128 bits is often enough to ensure that the hash is unique
         for i in 0..16 {
             hash_int = (hash_int << 8) + hash[i] as u128;
         }
@@ -31,9 +31,11 @@ impl BloomFilter {
         let mut hasher2 = Shabal192::new();
         hasher2.update(val);
         let hash2 = hasher2.finalize();
-        
+
         let mut hash_int_2: u128 = 0;
 
+        // hash is 32 bytes, so we need to convert it to a u128
+        // 128 bits is often enough to ensure that the hash is unique
         for i in 0..16 {
             hash_int_2 = (hash_int_2 << 8) + hash2[i] as u128;
         }
@@ -52,17 +54,16 @@ impl BloomFilter {
         buckets.push(bucket_2 as usize);
 
         return buckets;
-
     }
 
     pub fn new(size: usize) -> Self {
         BloomFilter {
             bitset: bitvec![u8, Msb0; 0; size],
-            size: size
+            size: size,
         }
     }
 
-    pub fn set(&mut self, val: &str) {
+    pub fn insert(&mut self, val: &str) {
         let buckets = self.get_buckets(val);
 
         for bucket in buckets {
@@ -75,6 +76,8 @@ impl BloomFilter {
 
         let mut exists = true;
 
+        // if any of the buckets are false, then the value does not exist
+        // if all of the buckets are true, then the value may exist (could be a false positive)
         for bucket in buckets {
             exists &= self.bitset.get(bucket).as_deref().unwrap();
         }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,38 @@
 use std::result;
 
+use bitvec::prelude::*;
 use rand::distributions::{Alphanumeric, DistString};
+use rand::rngs::StdRng;
+use rand::SeedableRng;
 use rand::{thread_rng, Rng};
 use std::mem;
-use bitvec::prelude::*;
 
 pub mod bloom_filter;
 
-fn generate_random_string(length: usize) -> String {
-    // todo - add seed
-    let string = Alphanumeric.sample_string(&mut rand::thread_rng(), length);
-    return string
+fn generate_random_string(length: usize, rng: &mut StdRng) -> String {
+    let string = Alphanumeric.sample_string(rng, length);
+    return string;
 }
 
 fn main() {
     // set candidates
     let mut candidates: Vec<String> = Vec::new();
-
+    let mut generator = StdRng::from_seed([0; 32]);
     const CANDIDATES: usize = 10_000;
 
     for _ in 0..CANDIDATES {
-        candidates.push(generate_random_string(24));
+        candidates.push(generate_random_string(24, &mut generator));
     }
 
     // // set half, test other half
     let mut bloom_filter = bloom_filter::BloomFilter::new(1_000_000);
 
-    for i in 0..CANDIDATES/2 {
-        bloom_filter.set(candidates.get(i).unwrap());
+    for i in 0..CANDIDATES / 2 {
+        bloom_filter.insert(candidates.get(i).unwrap());
     }
 
     let mut false_positives = 0;
-    for i in CANDIDATES/2..CANDIDATES {
+    for i in CANDIDATES / 2..CANDIDATES {
         let existance = bloom_filter.get(candidates.get(i).unwrap());
 
         if existance {
@@ -39,6 +40,6 @@ fn main() {
         }
     }
 
-    let fp_rate = false_positives as f32 / ((CANDIDATES/2) as f32) * 100.0;
+    let fp_rate = false_positives as f32 / ((CANDIDATES / 2) as f32) * 100.0;
     println!("Size: {CANDIDATES} / 2, FP rate: {fp_rate}%");
 }

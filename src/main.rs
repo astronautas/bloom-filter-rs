209f8f0af7f816500e3de1a00b17ce0910af4059
Lukas Valatka <lukas.valatka123@gmail.com>
2024-01-21 20:33:43 +0100

    stuff

--- src/main.rs
+++ src/main.rs
@@ -15,24 +15,26 @@ fn generate_random_string(length: usize, rng: &mut StdRng) -> String {
 }
 
 fn main() {
-    // set candidates
+    const CANDIDATES_COUNT: usize = 3;
     let mut candidates: Vec<String> = Vec::new();
     let mut generator = StdRng::from_seed([0; 32]);
-    const CANDIDATES: usize = 10_000;
 
-    for _ in 0..CANDIDATES {
+    // We'll prepare CANDIDATES_COUNT random strings
+    // and insert first CANDIDATES_COUNT / 2 of them into the bloom filter
+    // we'll then check the rest of the strings for existance in the bloom filter
+    // ideally, we should get 0 false positives
+    for _ in 0..CANDIDATES_COUNT {
         candidates.push(generate_random_string(24, &mut generator));
     }
 
-    // // set half, test other half
-    let mut bloom_filter = bloom_filter::BloomFilter::new(1_000_000);
+    let mut bloom_filter = bloom_filter::BloomFilter::new(8);
 
-    for i in 0..CANDIDATES / 2 {
+    for i in 0..CANDIDATES_COUNT / 2 {
         bloom_filter.insert(candidates.get(i).unwrap());
     }
 
     let mut false_positives = 0;
-    for i in CANDIDATES / 2..CANDIDATES {
+    for i in CANDIDATES_COUNT / 2..CANDIDATES_COUNT {
         let existance = bloom_filter.get(candidates.get(i).unwrap());
 
         if existance {
@@ -40,6 +42,7 @@ fn main() {
         }
     }
 
-    let fp_rate = false_positives as f32 / ((CANDIDATES / 2) as f32) * 100.0;
-    println!("Size: {CANDIDATES} / 2, FP rate: {fp_rate}%");
+    let fp_rate: f32 = false_positives as f32 / ((CANDIDATES_COUNT / 2) as f32) * 100.0;
+    println!("Size: {CANDIDATES_COUNT} / 2, FP rate: {fp_rate}%");
+    
 }
